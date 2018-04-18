a1905dc1a55b904ebfad55d93658288fb1a29a83
monpau <paumon64@PTPOR021.gunnebo.net>
2018-04-18 17:17:57 +0100

    alteração consulta individual do cliente arrayList movimentos NOK

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,16 @@ public class Main {
 	static ArrayList<Cliente> listaClientes = new ArrayList<Cliente>();
 
 	public static void main(String[] args) {
-		
-		 Cliente cliente1 = new Cliente("Mark Zuckerberg", "V");
-		 listaClientes.add(cliente1);
+
+		Cliente cliente1 = new Cliente("Mark Zuckerberg", "V");
+		listaClientes.add(cliente1);
 		// cliente1.getContaAOrdem().deposito(100000);
 		// cliente1.getContaAOrdem().deposito(99990);
 		// System.out.println(cliente1.getContaAOrdem().getMovimentos());
 		//
 		//
-		 Cliente cliente2 = new Cliente("Jeff Bezos", "O");
-		 listaClientes.add(cliente2);
+		Cliente cliente2 = new Cliente("Jeff Bezos", "O");
+		listaClientes.add(cliente2);
 		// cliente2.getContaAOrdem().deposito(250000);
 		// cliente2.getContaAOrdem().deposito(5555);
 		// System.out.println(cliente1.getContaAOrdem().getMovimentos());
@@ -24,16 +24,16 @@ public class Main {
 		// listaClientes.get(i).getContaAOrdem().deposito(-10);
 		// }
 
-		 Cliente cliente3 = new Cliente("Marissa Mayer ", "V");
-		 listaClientes.add(cliente3);
+		Cliente cliente3 = new Cliente("Marissa Mayer ", "V");
+		listaClientes.add(cliente3);
 		// Cliente cliente4 = new Cliente();
 		// cliente4.setNomeCliente("Larry Page");
 		// cliente4.setTipoConta("V");
 		// cliente4.setSaldo(2000000);
 		// listaClientes.add(cliente4);
 
-		 DisplayMenu();
-		 
+		DisplayMenu();
+
 		displayClientes();
 
 	}
@@ -49,7 +49,19 @@ public class Main {
 			System.out.println(listaClientes.get(i).getContaAOrdem().getSaldo());
 
 		}
+	}
 
+	public static void displayCliente(int id) {
+
+		System.out.println("Movimentos do cliente " + listaClientes.get(id).getNomeCliente());
+
+		for (int i = 0; i < listaClientes.size(); i++) {
+
+			System.out.println(listaClientes.get(id).getContaAOrdem().getMovimentos());
+
+		}
+
+		System.out.println(listaClientes.get(id).getContaAOrdem().getSaldo());
 	}
 
 	// public void ListofCustomers(){
@@ -70,6 +82,7 @@ public class Main {
 		String readOption;
 		String nomeCliente;
 		String tipoConta;
+		int oId;
 
 		do {
 
@@ -102,20 +115,34 @@ public class Main {
 				break;
 
 			case "2":
-				displayClientes();
+
+				int op = 0;
+				System.out.println("1. Listar todos clientes ? S/N ");
+				op = userInput.nextInt();
+				if (op == 1) {
+					displayClientes();
+					System.out.println("Press enter para continuar");
+					double movimento = userInput.nextInt();
+				} else {
+					System.out.println("Introduza o Id do cliente");
+					oId = userInput.nextInt();
+					displayCliente(oId);
+					System.out.println("Press enter para continuar");
+				}
+			//	listaClientes.get(oId-1).getContaAOrdem().getSaldo();
 				break;
 
-// alteração 17.4 by pm				
-				
+			// alteração 17.4 by pm
+
 			case "3":
 				// Options OptionsCliente = new Options();
 				System.out.println("1 - Depositos:");
 				System.out.println("2 - Levantamentos:");
-				int op = 0;
+				op = 0;
 				op = userInput.nextInt();
 				double movimento;
 				System.out.println("Introduza Id do cliente");
-				int oId = userInput.nextInt();
+				oId = userInput.nextInt();
 				if (op == 1) {
 					System.out.println("Introduza valor a Depositar");
 					movimento = userInput.nextInt();
@@ -124,7 +151,7 @@ public class Main {
 					movimento = userInput.nextInt();
 					movimento *= (-1);
 				}
-				listaClientes.get(oId-1).getContaAOrdem().deposito(movimento);
+				listaClientes.get(oId - 1).getContaAOrdem().deposito(movimento);
 				break;
 
 			case "4":
